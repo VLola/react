55c437fa6d0fd1e81c72c72804eda3a44f0b1eef
VLola <valiklolanew@gmail.com>
2023-03-04 22:00:34 +0200

    home page changed

--- project_118/src/pages/Home.js
+++ project_118/src/pages/Home.js
@@ -1,5 +1,13 @@
 const Home = () => {
-  return <h1>Home</h1>;
+  return <div className="d-flex flex-column justify-content-center align-items-center h-75 text-animate">
+    <div>Hello & Welcome</div>
+    <div>
+      <a href="https://github.com/VLola">
+        <div className="git">in</div>
+      </a>
+    </div>
+    <div>I am Valentyn Lola</div>
+  </div>;
 };
 
 export default Home;
